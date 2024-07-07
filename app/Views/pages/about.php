511e47f846a640cfcff60d3bade4e2d5cf55d909
zahratalitapr <zahratalitapr12@gmail.com>
2024-07-07 14:51:51 +0700

    menambahkan halaman home dan abaout

---
+++ app/Views/pages/about.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <h1>About Me</h1>
+            
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia velit veritatis consectetur, 
+                corrupti deserunt placeat consequuntur labore nisi sed animi praesentium sapiente, 
+                quas illo sunt dolorum nemo. Cum, ipsam doloremque!
+            </p>
+        </div>
+    </div>
+</div>
+    
+    
